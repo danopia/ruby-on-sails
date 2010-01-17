4cfe58d4496dffce4489cca406c12c985a715f5f
Daniel <danopia@ubuntu.ubuntu-domain>
2010-01-17 17:28:03 -0500

    sails.rb includes the server/client classes now too.

--- sails.rb
+++ sails.rb
@@ -1,7 +1,10 @@
-# Includes the lib/ files: wave_proto utils delta_builder operations playback provider
-# server remote wave blip annotation thread base_delta fake_delta delta database wave_user
+# Includes the lib/ files: wave_proto utils delta_builder operations playback
+# server wave blip annotation thread base_delta fake_delta delta database
+# wave_user server_list
 
 %w{wave_proto utils delta_builder operations playback server wave blip annotation thread base_delta fake_delta delta database wave_user server_list}.each do |file|
 	require File.join(File.dirname(__FILE__), 'lib', file)
 end
 
+require File.join(File.dirname(__FILE__), 'lib', 'protocol', 'client')
+require File.join(File.dirname(__FILE__), 'lib', 'protocol', 'server')
